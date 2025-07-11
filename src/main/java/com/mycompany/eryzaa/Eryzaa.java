10ff2bf05f1413672e3739a557bb1b40277dff92
CL2~PC13 <CL2~PC13@DESKTOP-BCGIHVQ>
2025-07-11 12:17:57 +0800

    sample program

---
+++ src/main/java/com/mycompany/eryzaa/Eryzaa.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.eryzaa;
+
+/**
+ *
+ * @author CL2~PC13
+ */
+public class Eryzaa {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        System.out.println("Eryza Galang");
+    }
+}
